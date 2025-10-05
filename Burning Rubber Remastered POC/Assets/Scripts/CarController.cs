fe49f14d0dbd9e839eaa5495f069b72388145a35
Jewel-Thomas <jewelthomas20@gmail.com>
2025-10-05 18:05:05 +0530

    added necessary functions in car controller

--- Burning Rubber Remastered POC/Assets/Scripts/CarController.cs
+++ Burning Rubber Remastered POC/Assets/Scripts/CarController.cs
@@ -3,12 +3,115 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+
+    private Rigidbody playerRb;
+
     [SerializeField] private WheelColliders wheelColliders;
     [SerializeField] private WheelMeshes wheelMeshes;
 
+    [SerializeField] private float gasInput;
+    [SerializeField] private float steeringInput;
+    [SerializeField] private float brakeInput;
+    [SerializeField] private AnimationCurve speedVsAngleCurve;
+    [SerializeField] private bool isReversing;
+
+    private float speed;
+
+    private enum DriveMode
+    {
+        Rear_Wheel_Drive,
+        Front_Wheel_Drive,
+        All_Wheel_Drive
+    }
+
+
+    [SerializeField] private float motorPower;
+    [SerializeField] private float brakePower;
+    [SerializeField] private DriveMode driveMode;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
+        speed = playerRb.velocity.magnitude;
+        GetInput();
+        isReversing = IsReversing();
+        Accelerate();
+        Steer();
+        Brake();
         UpdateAllWheels();
+    }
+
+
+    // TODO: Get input using the new Input System. Doing this only for initiating functionality.
+    private void GetInput()
+    {
+        gasInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+    }
+
+    private void Accelerate()
+    {
+        switch(driveMode)
+        {
+            default:
+            case DriveMode.Rear_Wheel_Drive:
+                AccelerateWheel(wheelColliders.rearLeftWheelCollider);
+                AccelerateWheel(wheelColliders.rearRightWheelCollider);
+                break;
+            case DriveMode.Front_Wheel_Drive:
+                AccelerateWheel(wheelColliders.frontLeftWheelCollider);
+                AccelerateWheel(wheelColliders.frontRightWheelCollider);
+                break;
+            case DriveMode.All_Wheel_Drive:
+                AccelerateWheel(wheelColliders.rearLeftWheelCollider);
+                AccelerateWheel(wheelColliders.rearRightWheelCollider);
+                AccelerateWheel(wheelColliders.frontLeftWheelCollider);
+                AccelerateWheel(wheelColliders.frontRightWheelCollider);
+                break;
+        }
+    }
+
+    private void AccelerateWheel(WheelCollider wheelCollider)
+    {
+        wheelCollider.motorTorque = motorPower * gasInput;
+    }
+
+    private void Steer()
+    {
+        float steeringAngle = speedVsAngleCurve.Evaluate(speed) * steeringInput;
+        wheelColliders.frontLeftWheelCollider.steerAngle = steeringAngle;
+        wheelColliders.frontRightWheelCollider.steerAngle = steeringAngle;
+    }
+
+    private bool IsReversing()
+    {
+        float slipAngle = Vector3.Angle(transform.forward, playerRb.velocity - transform.forward);
+        if(slipAngle < 120)
+        {
+            if(gasInput < 0)
+            {
+                brakeInput = Mathf.Abs(gasInput);
+                gasInput = 0;
+            }
+            return false;
+        }
+        else
+        {
+            brakeInput = 0;
+            return true;
+        }
+    }
+
+    private void Brake()
+    {
+        wheelColliders.frontLeftWheelCollider.brakeTorque = brakeInput * brakePower * 0.7f;
+        wheelColliders.frontRightWheelCollider.brakeTorque = brakeInput * brakePower * 0.7f;
+        wheelColliders.rearLeftWheelCollider.brakeTorque = brakeInput * brakePower * 0.3f;
+        wheelColliders.rearRightWheelCollider.brakeTorque = brakeInput * brakePower * 0.3f;
     }
 
     private void UpdateAllWheels()
